cb2e526e788562883d536d7d30df3909810c226f
Enos Recanati <recanatie@hotmail.it>
2020-02-14 00:24:10 +0100

    !!! iOS Build Errors !!! Android & UWP analysis almost completed, not audio response yet! Cognitive Services endpoint and keys needed in source code.

--- src/Rossy.App/Rossy.App/MainPage.xaml.cs
+++ src/Rossy.App/Rossy.App/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Plugin.Media;
 using Plugin.Media.Abstractions;
+using Rossy.IO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,22 +17,95 @@ namespace Rossy.App
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private Configuration AppConfiguration { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            AppConfiguration = new Configuration
+            {
+                GeordiConfig = new Geordi.Configuration
+                {
+                    Endpoint = "",
+                    SubscriptionKey = ""
+                },
+                ModemConfig = new Modem.Configuration
+                {
+                    Endpoint = "",
+                    Key = "",
+                    Region = ""
+                },
+                RosettaConfig = new Rosetta.Configuration
+                {
+                    AppIdEN = "",
+                    AppIdIT = "",
+                    Endpoint = "",
+                    PredictionKey = "",
+                    TextAnalysisEndpoint = "",
+                    TextAnalysisSubscriptionKey = ""
+                },
+                StorageConfig = new Storage.Configuration
+                {
+                    ConnectionString = "",
+                    ContainerName = ""
+                }
+            };
 
             TxtUtterance.Text = "what's up?";
             TxtFilePath.Text = "";
         }
 
+        private async void BtnAnalyze_Clicked(object sender, EventArgs e)
+        {
+            string blobUrl = string.Empty;
+            if (!string.IsNullOrWhiteSpace(TxtFilePath.Text))
+            {
+                blobUrl = TxtFilePath.Text;
+            }
+            else
+            {
+                await TakePicture();
+            }
+
+            string utterance;
+            if (!string.IsNullOrWhiteSpace(TxtUtterance.Text))
+                utterance = TxtUtterance.Text;
+            else
+            {
+                utterance = "what's up?";
+            }
+
+            var analyzer = new Geordi(AppConfiguration);
+            Geordi.AnalysisResult response = analyzer.Analyze(blobUrl, utterance);
+
+            TxtAnalysisResult.Text = response.Log;
+            DeletePicture(blobUrl);
+        }
+
         private async void BtnPickFile_Clicked(object sender, EventArgs e)
+        {
+            await PickFile();
+        }
+
+        private async void BtnTakePicture_Clicked(object sender, EventArgs e)
+        {
+            await TakePicture();
+        }
+
+        private void DeletePicture(string blobUrl)
+        {
+            var storageManager = new Storage(AppConfiguration.StorageConfig);
+            var blobUri = new Uri(blobUrl);
+            storageManager.DeleteFile(blobUri);
+        }
+
+        private async Task PickFile()
         {
             await CrossMedia.Current.Initialize();
 
             if (!CrossMedia.Current.IsPickPhotoSupported)
             {
                 await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
-
                 return;
             }
             var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
@@ -42,23 +117,36 @@ namespace Rossy.App
             if (file == null)
                 return;
 
+            var blobUrl = UploadPicture(file);
+
             CurrentImage.Source = ImageSource.FromStream(() =>
             {
                 var stream = file.GetStream();
                 file.Dispose();
-
                 return stream;
             });
         }
 
-        private async void BtnTakePicture_Clicked(object sender, EventArgs e)
+        private string UploadPicture(MediaFile file)
+        {
+            var extension = file.Path.Substring(file.Path.LastIndexOf('.'));
+
+            using (Stream stream = file.GetStream())
+            {
+                var storageManager = new Storage(AppConfiguration.StorageConfig);
+                var blobUrl = storageManager.UploadFile(stream, extension);
+                TxtFilePath.Text = blobUrl;
+                return blobUrl;
+            }
+        }
+
+        private async Task TakePicture()
         {
             await CrossMedia.Current.Initialize();
 
             if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
             {
                 await DisplayAlert("No Camera", ":( No camera available.", "OK");
-
                 return;
             }
 
@@ -74,24 +162,18 @@ namespace Rossy.App
             });
 
             if (file == null)
-            {
                 return;
-            }
 
             await DisplayAlert("File Location", file.Path, "OK");
+
+            var blobUrl = UploadPicture(file);
 
             CurrentImage.Source = ImageSource.FromStream(() =>
             {
                 var stream = file.GetStream();
                 file.Dispose();
-
                 return stream;
             });
-        }
-
-        private void BtnAnalyze_Clicked(object sender, EventArgs e)
-        {
-
         }
     }
 }
